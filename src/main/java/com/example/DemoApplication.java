d064ab86ef0ef67a9300c3bde7954bc2dc86f680
Cyrille Le Clerc <cleclerc@cloudbees.com>
2017-07-09 22:38:07 +0200

    [MYAPP-123] add Get mapping on "/"

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -2,12 +2,20 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
         // FIXME this is a fix me message for the Jenkins Tasks Scanner plugin
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String home() {
+		return "Hello World!";
 	}
 }
